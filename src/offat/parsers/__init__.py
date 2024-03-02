6e152daba9a22582d8eba5cef53a8100c32c68ea
Dhrumil Mistry <56185972+dmdhrumilmistry@users.noreply.github.com>
2024-03-03 01:09:35 +0530

    fix url parsing bugs

--- src/offat/parsers/__init__.py
+++ src/offat/parsers/__init__.py
@@ -1,9 +1,11 @@
+from sys import exit
 from requests import get as http_get
 from json import loads as json_load, JSONDecodeError
 from .openapi import OpenAPIv3Parser
 from .swagger import SwaggerParser
 from .parser import BaseParser
 from ..utils import is_valid_url
+from ..logger import logger
 
 
 def create_parser(fpath_or_url: str, spec: dict = None) -> SwaggerParser | OpenAPIv3Parser:
@@ -11,14 +13,16 @@ def create_parser(fpath_or_url: str, spec: dict = None) -> SwaggerParser | OpenA
     if fpath_or_url and is_valid_url(fpath_or_url):
         res = http_get(fpath_or_url)
         if res.status_code != 200:
-            raise ValueError(
-                f"server returned status code {res.status_code} offat expects 200 status code"
-            )
+            logger.error(
+                "server returned status code %d offat expects 200 status code", res.status_code)
+            exit(-1)
+
         try:
             spec = json_load(res.text)
             fpath_or_url = None
-        except JSONDecodeError as e:
-            raise ValueError("Invalid json data spec file url")
+        except JSONDecodeError:
+            logger.error("Invalid json data spec file url")
+            exit(-1)
 
     parser = BaseParser(file_or_url=fpath_or_url, spec=spec)
     if parser.is_v3:
